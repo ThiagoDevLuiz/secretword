0f52b7f8b8cedd13c67f55d60fe0e7f6d369f2c6
Thiago Luiz <110872096+ThiagoDevLuiz@users.noreply.github.com>
2023-05-07 13:01:21 -0300

    Funcionamento das trocas de stages e pegar palavras

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import './GameOver.css'
+import React from 'react';
+import './GameOver.css';
 
-const GameOver = () => {
+const GameOver = ({ retry }) => {
   return (
-    <div>GameOver</div>
-  )
-}
+    <div>
+      <h1>GameOver</h1>
+      <button onClick={retry}>Resetar jogo</button>
+    </div>
+  );
+};
 
-export default GameOver
+export default GameOver;
